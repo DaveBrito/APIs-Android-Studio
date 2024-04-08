c13995d1cca5ae33b2c0b202434914d826b13e66
davi <davibritojunior1@gmail.com>
2024-04-08 16:19:20 -0300

    `fix` : Corrigido e adicionado comentários

--- app/src/main/java/com/example/tempo/MainActivity.java
+++ app/src/main/java/com/example/tempo/MainActivity.java
@@ -25,84 +25,134 @@ import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText etCity, etCountry;
-    TextView tvResult;
+    // Declarando as variáveis ​​de classe
+    EditText etCity, etCountry; // Campos de entrada de texto para cidade e país
+    TextView tvResult; // Campo de texto para exibir os resultados
 
+    // Coordenadas geográficas padrão (São Paulo, Brasil)
     double latitude = -23.5475;
     double longitude = -46.6361;
+
+    // Número de previsões meteorológicas a serem recuperadas
     int numberOfForecasts = 5;
 
+    // URL da API do OpenWeatherMap
     String url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&cnt=" + numberOfForecasts + "&appid=a2ffacee9f355be08ed01d8ab9322df9";
 
+    // Chave de acesso à API do OpenWeatherMap
     private final String appid = "a2ffacee9f355be08ed01d8ab9322df9";
 
+    // Formato decimal para formatar os valores numéricos
     DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etCity = findViewById(R.id.etCity);
-        etCountry = findViewById(R.id.etCountry);
-        tvResult = findViewById(R.id.tvResult);
+
+        // Inicialização dos campos de entrada e saída de texto
+        etCountry = findViewById(R.id.etCountry); // Campo de entrada de texto para o país
+        tvResult = findViewById(R.id.tvResult); // Campo de texto para exibir os resultados
     }
 
+    // Método chamado quando o botão é clicado para obter os detalhes do clima
     public void getWeatherDetails(View view) {
-        String tempurl = "";
-        String city = etCity.getText().toString().trim();
-        String country = etCountry.getText().toString().trim();
+        String tempurl = ""; // Variável para armazenar a URL da solicitação
+        String country = etCountry.getText().toString().trim(); // Obtendo o nome do país do campo de entrada
 
-        if (TextUtils.isEmpty(city)) {
-            tvResult.setText("Cidade não encontrada");
+        // Verificando se o campo de entrada do país está vazio
+        if (TextUtils.isEmpty(country)) {
+            // Se estiver vazio, exibir uma mensagem de erro na TextView de resultados
+            tvResult.setText("Por favor, insira o nome do país");
         } else {
-            if (!TextUtils.isEmpty(country)) {
-                tempurl = url + "&q=" + city + "," + country + "&appid=" + appid;
-            } else {
-                tempurl = url + "&q=" + city + "&appid=" + appid;
-            }
+            // Se não estiver vazio, construir a URL da solicitação com base no país fornecido
+            tempurl = url + "&q=" + country + "&appid=" + appid;
 
+            // Criando uma solicitação de string usando Volley
             StringRequest stringRequest = new StringRequest(Request.Method.GET, tempurl, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
+                    // Método chamado quando a resposta da solicitação é recebida com sucesso
+
+                    // Variável para armazenar a saída a ser exibida na TextView de resultados
                     String output = "";
+
                     try {
                         JSONObject jsonResponse = new JSONObject(response);
-                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
-                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
-                        String description = jsonObjectWeather.getString("description");
-                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
-                        double temp = jsonObjectMain.getDouble("temp") - 273.15;
-                        double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
-                        float pressure = jsonObjectMain.getInt("pressure");
-                        int humidity = jsonObjectMain.getInt("humidity");
-                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
-                        String wind = jsonObjectWind.getString("speed");
-                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
-                        String clouds = jsonObjectClouds.getString("all");
-                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
-                        String countryName = jsonObjectSys.getString("country");
-                        String cityName = jsonResponse.getString("name");
-                        tvResult.setTextColor(Color.rgb(68,134,199));
-                        output += "Current weather of " + cityName + " (" + countryName + ")"
-                                + "\n Temp: " + df.format(temp) + " °C"
-                                + "\n Feels Like: " + df.format(feelsLike) + " °C"
-                                + "\n Humidity: " + humidity + "%"
-                                + "\n Description: " + description
-                                + "\n Wind Speed: " + wind + "m/s (meters per second)"
-                                + "\n Cloudiness: " + clouds + "%"
-                                + "\n Pressure: " + pressure + " hPa";
-                        tvResult.setText(output);
+
+                        // Verificando se a resposta indica que o país ou estado não foi encontrado
+                        if (jsonResponse.has("message") && jsonResponse.getString("message").equals("city not found")) {
+                            // Se o país ou estado não for encontrado, exibir uma mensagem de erro na TextView de resultados
+                            tvResult.setText("País ou estado não encontrado");
+                        } else {
+                            // Obtendo a matriz de informações sobre o clima da resposta
+                            JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+
+                            // Obtendo o primeiro objeto de informações sobre o clima
+                            JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+
+                            // Obtendo a descrição do clima
+                            String description = jsonObjectWeather.getString("description");
+
+                            // Obtendo o objeto de informações principais sobre o clima
+                            JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+
+                            // Obtendo a temperatura e ajustando de Kelvin para Celsius
+                            double temp = jsonObjectMain.getDouble("temp") - 273.15;
+
+                            // Obtendo a sensação térmica e ajustando de Kelvin para Celsius
+                            double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+
+                            // Obtendo a pressão atmosférica
+                            float pressure = jsonObjectMain.getInt("pressure");
+
+                            // Obtendo a umidade relativa do ar
+                            int humidity = jsonObjectMain.getInt("humidity");
+
+                            // Obtendo informações sobre o vento
+                            JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                            String wind = jsonObjectWind.getString("speed");
+
+                            // Obtendo informações sobre a nebulosidade
+                            JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                            String clouds = jsonObjectClouds.getString("all");
+
+                            // Obtendo informações sobre o país e a cidade
+                            JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                            String countryName = jsonObjectSys.getString("country");
+                            String cityName = jsonResponse.getString("name");
+
+                            // Definindo a cor do texto na TextView de resultados
+                            tvResult.setTextColor(Color.rgb(68, 134, 199));
+
+                            // Construindo a saída com os detalhes do clima
+                            output += "Clima atual de " + cityName + " (" + countryName + ")"
+                                    + "\n Temperatura: " + df.format(temp) + " °C"
+                                    + "\n Sensação: " + df.format(feelsLike) + " °C"
+                                    + "\n Umidade: " + humidity + "%"
+                                    + "\n Descrição: " + description
+                                    + "\n Velocidade do Vento: " + wind + "m/s (metros por segundo)"
+                                    + "\n Nebulosidade: " + clouds + "%"
+                                    + "\n Pressão: " + pressure + " hPa";
+
+                            // Exibindo a saída na TextView de resultados
+                            tvResult.setText(output);
+                        }
                     } catch (JSONException e) {
+                        // Lidando com exceções ao analisar a resposta JSON
                         e.printStackTrace();
                     }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
+                    // Método chamado quando ocorre um erro na solicitação
+                    // Exibindo uma mensagem de erro usando Toast
                     Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
                 }
             });
 
+            // Adicionando a solicitação à fila de solicitações do Volley
             RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
             requestQueue.add(stringRequest);
         }
